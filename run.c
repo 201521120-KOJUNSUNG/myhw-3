f47ac7eb4b8a365a7ed782f44fc7b2402c6da8cb
201521120-KOJUNSUNG <rhwnstjd@ajou.ac.kr>
2017-12-19 23:48:31 +0900

    iddd

--- run.c
+++ run.c
@@ -1,48 +1,111 @@
 #include <sys/types.h>
 #include <limits.h>
-
+#include <sys/resource.h>
+#include <unistd.h>
+#include <sys/time.h>
 #include "run.h"
 #include "util.h"
-
+void *last = 0;
 void *base = 0;
+void *my_brk;
+struct rlimit my_rlimit;
 
 p_meta find_meta(p_meta *last, size_t size) {
+  
   p_meta index = base;
-  p_meta result = base;
+  p_meta result = -1;
+  
+ if(last==base)return result;
 
   switch(fit_flag){
     case FIRST_FIT:
-    {
+    {   
       //FIRST FIT CODE
+	while(index){
+		if((index->free) && (index->size>=size)) result = index;
+		if(index->next==NULL)result = index;
+		index = index->next;
+	}
+
     }
     break;
 
     case BEST_FIT:
     {
+     
       //BEST_FIT CODE
+      while(index){
+	if((index->free) && (index->size>=size)){
+	 if(result ->size > index->size)result = index;
+}
+index=index->next;
+}
+
     }
     break;
 
     case WORST_FIT:
     {
-      //WORST_FIT CODE
-    }
-    break;
-
+	while(index){
+	
+	if((index->free)&& index->size>=size){ 
+	if(result->size<index->size) result = index;
+	index=index->next;
   }
+    
+ }
+}
+ break;
+ }
   return result;
 }
-
 void *m_malloc(size_t size) {
+ int total_size;
+ p_meta result = find_meta(&last, size);
+  size =((size+3)/4)*4;
+ total_size = size + META_SIZE;
+  base = sbrk(0);
+  last = base;
+if(result == -1 || (!result->next && (!result->free || result->free && result->size<size))){
+ p_meta temp = last;
+ last += total_size;
+ if(brk(last)==-1) return((void*)0);
+ temp->free = 0;
+ temp->next =0;
+ temp ->prev = result;
+ temp ->size = size;
+ if(result!=-1) result->next = temp;
+ result = temp;
 
+ }
+return result ->data;
+   
 }
 
 void m_free(void *ptr) {
-
+   p_meta index = ptr -META_SIZE;
+   index->free = 1;
+ if(index->next && index->next->free == 1) {
+ index->size += index->next->size + META_SIZE;
+ index->next = index->next->next;
+}
+ if(index->prev!=-1){
+ if(index->prev->free){
+ index= index->prev;
+ index->size += index->next->size+META_SIZE;
+ index->next = index->next->next;
+}
+ if(!index->next){
+ last = index->size + META_SIZE;
+ index->prev->next = 0;
+}
+}
+else if(!index->next && !index->prev){
+last = base;
+}
+ptr = 0;
 }
 
-void*
-m_realloc(void* ptr, size_t size)
-{
+void *m_realloc(void* ptr, size_t size){
 
 }
